3bf9053b6241acaa7f20a0bef5d0321aee200594
Cristiano Morais <cristianopdmorais@gmail.com>
2024-01-07 11:46:03 -0300

    Mod09Err

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {    
+    $('header button').click(function() {            
+        $('form').slideDown();
+    })
+
+    $('#BotaoCancelar').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefaut();
+        const endNovaImagem = $('#EnderecoImagemNova').val();
+        const NovoItem = $('<li style="display: none"></li>');
+        $(`<img src="${endNovaImagem}" />`).appendTo(NovoItem);
+        $(`
+            <div class="overlay-imagem">
+                <a href="${endNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                    ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(NovoItem);
+        $(NovoItem).appendTo('ul');
+        $('#EnderecoImagemNova').val('');
+    
+    })
+})
