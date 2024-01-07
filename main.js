aef278c9ff3441105e0d9580a66ba8bc3ad2e9eb
Cristiano Morais <cristianopdmorais@gmail.com>
2024-01-07 11:58:56 -0300

    Mod09

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ $(document).ready(function() {
         $('form').slideDown();
     })
 
-    $('#BotaoCancelar').click(function() {
+    $('#BotaoCancelar').click(function(){
         $('form').slideUp();
     })
 
     $('form').on('submit', function(e) {
         e.preventDefaut();
         const endNovaImagem = $('#EnderecoImagemNova').val();
-        const NovoItem = $('<li style="display: none"></li>');
+        const NovoItem = $('<li></li>');
         $(`<img src="${endNovaImagem}" />`).appendTo(NovoItem);
         $(`
             <div class="overlay-imagem">
                 <a href="${endNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
-                    ver imagem em tamanho real
+                    Ver imagem em tamanho real
                 </a>
             </div>
         `).appendTo(NovoItem);
